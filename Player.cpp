58bb622cfe2a33f587e15e3fab5a77f13f0a3584
sabatinySingularity <sabatiny@singularitysystems.net>
2026-09-05 14:11:52 -0300

    Make a better collision for the grounded check

--- Player.cpp
+++ Player.cpp
@@ -3,82 +3,84 @@
 #include "Settings.h"
 
 Player::Player(float x, float y, float width, float height)
-    : Entity(x, y, width, height, SDL_FColor{ 0.0f, 1.0f, 0.0f, 1.0f })
+	: Entity(x, y, width, height, SDL_FColor{ 0.0f, 1.0f, 0.0f, 1.0f })
 {
 }
 
 void Player::handleInput() {
-    const bool* keyState = SDL_GetKeyboardState(NULL);
+	const bool* keyState = SDL_GetKeyboardState(NULL);
 
-    xVelocity = 0.0f;
+	xVelocity = 0.0f;
 
-    if (keyState[SDL_SCANCODE_A] || keyState[SDL_SCANCODE_LEFT]) {
-        xVelocity = -speed;
-    }
-    if (keyState[SDL_SCANCODE_D] || keyState[SDL_SCANCODE_RIGHT]) {
-        xVelocity = speed;
-    }
+	if (keyState[SDL_SCANCODE_A] || keyState[SDL_SCANCODE_LEFT]) {
+		xVelocity = -speed;
+	}
+	if (keyState[SDL_SCANCODE_D] || keyState[SDL_SCANCODE_RIGHT]) {
+		xVelocity = speed;
+	}
 
-    if ((keyState[SDL_SCANCODE_SPACE] || keyState[SDL_SCANCODE_UP]) && isGrounded) {
-        yVelocity = jumpForce;
-        isGrounded = false;
-    }
+	if ((keyState[SDL_SCANCODE_SPACE] || keyState[SDL_SCANCODE_UP]) && isGrounded) {
+		yVelocity = jumpForce;
+		isGrounded = false;
+	}
+}
+
+static bool isPointInsideRect(float px, float py, const SDL_FRect& rect) {
+	return (px >= rect.x && px <= rect.x + rect.w &&
+		py >= rect.y && py <= rect.y + rect.h);
 }
 
 void Player::update(float deltaTime)
 {
-    handleInput();
+	handleInput();
 
-    rect.x += xVelocity * deltaTime;
+	rect.x += xVelocity * deltaTime;
 
-    if (rect.x < 0.0f) {
-        rect.x = 0.0f;
-    }
-    else if (rect.x + rect.w > Settings::SCREEN_WIDTH) {
-        rect.x = Settings::SCREEN_WIDTH - rect.w;
-    }
+	if (rect.x < 0.0f) rect.x = 0.0f;
+	if (rect.x + rect.w > Settings::SCREEN_WIDTH) rect.x = Settings::SCREEN_WIDTH - rect.w;
 
-    for (const auto& entity : worldEntities) {
-        if (entity.get() == this) continue;
+	yVelocity += gravity * deltaTime;
+	rect.y += yVelocity * deltaTime;
 
-        SDL_FRect otherRect = entity->rect;
+	isGrounded = false;
 
-        if (SDL_HasRectIntersectionFloat(&rect, &otherRect)) {
-            if (rect.y + rect.h > otherRect.y + 2.0f && rect.y < otherRect.y + otherRect.h - 2.0f) {
-                if (xVelocity > 0.0f) {
-                    rect.x = otherRect.x - rect.w;
-                }
-                else if (xVelocity < 0.0f) {
-                    rect.x = otherRect.x + otherRect.w;
-                }
-            }
-        }
-    }
+	if (yVelocity >= 0.0f) {
 
-    yVelocity += gravity * deltaTime;
-    rect.y += yVelocity * deltaTime;
+		float footLeftX = rect.x + 2.0f;
+		float footRightX = rect.x + rect.w - 2.0f;
+		float footY = rect.y + rect.h + 1.0f;
 
-    isGrounded = false;
+		for (const auto& entity : worldEntities) {
+			if (entity.get() == this) continue;
 
-    for (const auto& entity : worldEntities) {
-        if (entity.get() == this) continue;
+			const SDL_FRect& platform = entity->rect;
 
-        SDL_FRect otherRect = entity->rect;
+			bool leftFootOnPlatform = isPointInsideRect(footLeftX, footY, platform);
+			bool rightFootOnPlatform = isPointInsideRect(footRightX, footY, platform);
 
-        if (SDL_HasRectIntersectionFloat(&rect, &otherRect)) {
-            if (yVelocity > 0.0f) {
-                
-                if (rect.y + rect.h - yVelocity * deltaTime <= otherRect.y + 12.0f) {
-                    rect.y = otherRect.y - rect.h;
-                    yVelocity = 0.0f;
-                    isGrounded = true;
-                }
-            }
-            else if (yVelocity < 0.0f) {
-               
-                rect.y = otherRect.y + otherRect.h;
-                yVelocity = 0.0f;
-            }
-        }
-    }
+			if (leftFootOnPlatform || rightFootOnPlatform) {
+				rect.y = platform.y - rect.h;
+				yVelocity = 0.0f;
+				isGrounded = true;
+				break;
+			}
+		}
+	}
+	else if (yVelocity < 0.0f) {
+		float headCenterX = rect.x + (rect.w / 2.0f);
+		float headY = rect.y - 1.0f;
+
+		for (const auto& entity : worldEntities) {
+			if (entity.get() == this) continue;
+
+			const SDL_FRect& platform = entity->rect;
+
+			if (isPointInsideRect(headCenterX, headY, platform)) {
+
+				rect.y = platform.y + platform.h;
+				yVelocity = 0.0f;
+				break;
+			}
+		}
+	}
 }
